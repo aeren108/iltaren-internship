09c3ab8e223b9464459ffc0102656606e6221ecf
aeren108 <aeren108@outlook.com>
2023-08-15 09:45:15 +0300

    handle misses

--- Algorithm/src/bus_guesser.cpp
+++ Algorithm/src/bus_guesser.cpp
@@ -68,11 +68,14 @@ int main() {
 			int est_period = temparr[j];
 			int min_period = est_period * (1 - ERROR_RATE);
 			int max_period = est_period * (1 + ERROR_RATE);
-
+			
+			
 			for (est_period = min_period; est_period <= max_period; ++est_period) { //Try all possible values for period
-				int length = 1;
-				Vector<int, 50> visited_tmp; visited_tmp.push_back(i); visited_tmp.push_back(j);
-
+				int length = 1, real_length = 1; int missed_index = -1; int missed_order = -1;
+				bool can_miss = true;
+				Vector<int, 50> visited_tmp; 
+				visited_tmp.push_back(i); visited_tmp.push_back(j);
+				
 				//Measure the pattern length for period with jth item (estimated period <- temparr[j])
 				for (int k = j + 1; k < temparr.size(); ++k) {
 					if (marks[k]) continue;
@@ -80,23 +83,36 @@ int main() {
 					int expected_time = (length + 1) * est_period;
 					int err = est_period * ERROR_RATE;
 
-					if (temparr[k] > expected_time + err) break; //There is no possible value for rest of the array
+					if (temparr[k] > expected_time + err) {
+						if (!can_miss) break; //There is no possible value for rest of the array
+						else {
+							length++;
+							missed_order = length;
+							can_miss = false;
+							k--;
+							continue;
+						}
+					} 
 					else if (temparr[k] <= expected_time + err && temparr[k] >= expected_time - err) {
 						length++;
-						//est_period = (est_period * (length - 1) + temparr[k] / length) / length; // adjust estimated period
+						real_length++;
 						visited_tmp.push_back(k);
 					}
+
 				}
 
-				//Set the period for the pattern with maximum length
+				if (missed_order == length - 1) { visited_tmp.remove(visited_tmp.size() - 1); length--; }
+				else if (missed_order == length - 2) { visited_tmp.remove(visited_tmp.size() - 1); visited_tmp.remove(visited_tmp.size() - 1); length-=2; }
 				if (length > maxlen) { maxlen = length; visited = visited_tmp; period = est_period; }
 			}
 		}
 
-		std::cout << "Max length for i=" << i << ": " << maxlen << std::endl << std::endl;
+		std::cout << "Max length for i=" << i << ": " << maxlen << std::endl << "PERIOD: " << period << " -> ";
 
 		//Mark visited elements
-		for (int j = 0; j < visited.size(); ++j) marks[visited[j]] = true;
+		for (int j = 0; j < visited.size(); ++j) { std::cout << times[visited[j]] << ", "; marks[visited[j]] = true; }
+		std::cout << std::endl << std::endl;
+		
 		
 		/*int length = 0;
 		for (int j = i; j < temparr.size(); ++j) {
@@ -112,8 +128,8 @@ int main() {
 		}*/
 		g.push_back(visited);
 		period = period == 0 ? times[i] : period;
-
-		int index = -1; //Find the pattern group if it already exists
+		periods.push_back(period);
+		/*int index = -1; //Find the pattern group if it already exists
 		for (int j = 0; j < periods.size(); ++j) {
 			int err = period * ERROR_RATE < periods[j] * ERROR_RATE ? period * ERROR_RATE + 0.5f : periods[j] * ERROR_RATE + 0.5f;
 			if (period <= periods[j] + err && period >= periods[j] - err) index = j;
@@ -125,7 +141,7 @@ int main() {
 			periods.push_back(period);
 			groups.push_back(Vector<int, 10>());
 			groups[groups.size() - 1].push_back(i);
-		}
+		}*/
 	}
 
 	std::cout << "BUS COUNT: " << periods.size() << std::endl;
