b98cac10464335a7fe07a507c77507f945625ac1
aeren108 <aeren108@outlook.com>
2023-08-15 11:11:52 +0300

    extract groups

--- Algorithm/src/bus_guesser.cpp
+++ Algorithm/src/bus_guesser.cpp
@@ -2,10 +2,14 @@
 #include "vector.h"
 #include "list.h"
 
-#define ERROR_RATE 0.1f
+#define MAX_BUS 50
+#define MAX_GROUP 15
 
-Vector<int, 50> times;
-Vector<bool, 50> marks;
+#define ERROR_RATE 0.1f
+#define RATE_OF_CHANGE 0.05f
+
+Vector<int, MAX_BUS> times;
+Vector<bool, MAX_BUS> marks;
 
 void read_input() {
 	int min; std::cin >> min;
@@ -30,38 +34,84 @@ void preprocess_times() {
 	}
 }
 
-void subtract_element(int i, Vector<int, 50>& arr) {
+void subtract_element(int i, Vector<int, MAX_BUS>& arr) {
 	int temp = arr[i];
 	for (int i = 0; i < arr.size(); ++i) arr[i] -= temp;
 }
 
-int abs(int x) {
-	if (x < 0) return -x; return x;
-}
+int abs(int x) { if (x < 0) return -x; return x; }
 
-int main() {
-	Vector<int, 50> periods;
-	Vector<Vector<int, 10>, 50> groups;
-	Vector<Vector<int, 50>, 10> g;
-
-	read_input();
+void extract_recurring_groups(Vector<Vector<int, MAX_BUS>, MAX_GROUP>& groups, Vector<int, MAX_BUS>& periods) {
 	for (int i = 0; i < times.size(); ++i) {
-		marks.push_back(false);
-	}
-
-	//preprocess_times();
-
-	for (int i = 0; i < times.size(); ++i) { //Find a pattern for i'th element
 		if (marks[i]) continue;
 		
-		int maxlen = 0; int period = 0;
-		Vector<int, 50> temparr = times;
-		
+		int maxlen = 0;
+		int period = 0;
+
+		Vector<int, MAX_BUS> temparr = times;;
+		Vector<int, MAX_BUS> visited;
+
 		subtract_element(i, temparr);
 
 		std::cout << "Times: " << temparr << std::endl;
 		std::cout << "Checked: " << marks << std::endl;
-		Vector<int, 50> visited;
+
+		for (int j = i + 1; j < temparr.size(); ++j) {
+			if (marks[j]) continue;
+
+			int est_period = temparr[j];
+			int length = 1;
+			int prev = -1;
+
+			Vector<int, MAX_BUS> visited_tmp;
+			visited_tmp.push_back(i);
+			visited_tmp.push_back(j);
+
+			for (int k = j + 1; k < temparr.size(); ++k) {
+				int max_expected_time = est_period + est_period * RATE_OF_CHANGE;
+
+				if (temparr[k] > max_expected_time) break; //There is no possible value for rest of the array
+				else {
+					length++;
+					visited_tmp.push_back(k);
+					est_period = temparr[k] - prev;
+					prev = temparr[k];
+				}
+			}
+
+			if (length > maxlen) {
+				maxlen = length;
+				visited = visited_tmp;
+				period =  times[visited[1]] - times[visited[0]];
+			}
+		}
+
+		for (int j = 0; j < visited.size(); ++j) {
+			std::cout << times[visited[j]] << ", ";
+			marks[visited[j]] = true;
+		} std::cout << std::endl << std::endl;
+
+		periods.push_back(period);
+		groups.push_back(visited);
+	}
+
+}
+
+void extract_groups(Vector<Vector<int, MAX_BUS>, MAX_GROUP>& groups, Vector<int, MAX_BUS>& periods) {
+	for (int i = 0; i < times.size(); ++i) { //Find a pattern for i'th element
+		if (marks[i]) continue;
+
+		int maxlen = 0; 
+		int period = 0;
+
+		Vector<int, MAX_BUS> temparr = times;
+		Vector<int, MAX_BUS> visited;
+
+		subtract_element(i, temparr);
+
+		std::cout << "Times: " << temparr << std::endl;
+		std::cout << "Checked: " << marks << std::endl;
+
 		for (int j = i + 1; j < temparr.size(); ++j) { //Check for the pattern with maximum length
 			if (marks[j]) continue;
 
@@ -71,7 +121,10 @@ int main() {
 
 			for (est_period = min_period; est_period <= max_period; ++est_period) { //Try all possible values for period
 				int length = 1;
-				Vector<int, 50> visited_tmp; visited_tmp.push_back(i); visited_tmp.push_back(j);
+				Vector<int, MAX_BUS> visited_tmp;
+
+				visited_tmp.push_back(i);
+				visited_tmp.push_back(j);
 
 				//Measure the pattern length for period with jth item (estimated period <- temparr[j])
 				for (int k = j + 1; k < temparr.size(); ++k) {
@@ -83,7 +136,6 @@ int main() {
 					if (temparr[k] > expected_time + err) break; //There is no possible value for rest of the array
 					else if (temparr[k] <= expected_time + err && temparr[k] >= expected_time - err) {
 						length++;
-						//est_period = (est_period * (length - 1) + temparr[k] / length) / length; // adjust estimated period
 						visited_tmp.push_back(k);
 					}
 				}
@@ -93,72 +145,40 @@ int main() {
 			}
 		}
 
-		std::cout << "Max length for i=" << i << ": " << maxlen << std::endl << std::endl;
+		std::cout << "Max length for i=" << i << ": " << maxlen << std::endl << "Period: " << period << " -> ";
 
 		//Mark visited elements
-		for (int j = 0; j < visited.size(); ++j) marks[visited[j]] = true;
-		
-		/*int length = 0;
-		for (int j = i; j < temparr.size(); ++j) {
-			if (marks[j]) continue;
-			int expected_time = length * period;
-			float err = period / 8.0f;
+		for (int j = 0; j < visited.size(); ++j) {
+			std::cout << times[visited[j]] << ", ";
+			marks[visited[j]] = true;
+		} std::cout << std::endl << std::endl;
 
-			if (temparr[j] > expected_time + err) break;
-			else if (temparr[j] <= expected_time + err && temparr[j] >= expected_time - err) {
-				marks[j] = true;
-				length++;
-			}
-		}*/
-		g.push_back(visited);
 		period = period == 0 ? times[i] : period;
-
-		int index = -1; //Find the pattern group if it already exists
-		for (int j = 0; j < periods.size(); ++j) {
-			int err = period * ERROR_RATE < periods[j] * ERROR_RATE ? period * ERROR_RATE + 0.5f : periods[j] * ERROR_RATE + 0.5f;
-			if (period <= periods[j] + err && period >= periods[j] - err) index = j;
-		}
-
-		if (index != -1) { //If a group with same period exists, push it to the existing group
-			groups[index].push_back(i);
-		} else { //If it does not exist push new
-			periods.push_back(period);
-			groups.push_back(Vector<int, 10>());
-			groups[groups.size() - 1].push_back(i);
-		}
+		periods.push_back(period);
+		groups.push_back(visited);
 	}
+}
+
+int main() {
+	Vector<int, MAX_BUS> periods;
+	Vector<Vector<int, MAX_BUS>, MAX_GROUP> groups;
+
+	read_input();
+	for (int i = 0; i < times.size(); ++i) {
+		marks.push_back(false);
+	}
+
+	//preprocess_times();
+
+	extract_recurring_groups(groups, periods);
 
 	std::cout << "BUS COUNT: " << periods.size() << std::endl;
 	std::cout << "PERIODS: " << periods << std::endl;
-
 	std::cout << "GROUPS: " << std::endl;
 
-	/*for (int i = 0; i < groups.size(); ++i) {
-		
-		for (int k = 0; k < groups[i].size(); ++k) {
-			int length = 0;
-
-			for (int j = 0; j < times.size(); ++j) {
-				int expected_time = times[groups[i][k]] + length * periods[i];
-				int err = periods[i] * ERROR_RATE;
-
-				if (abs(times[j] - expected_time) <= err) {
-					std::cout << times[j] << ", ";
-					length++;
-				}
-			}
-			
-		}
-
-		std::cout << std::endl;
-	}
-	std::cout << std::endl;*/
-
-	for (int i = 0; i < g.size(); ++i) {
-		for (int j = 0; j < g[i].size(); ++j) {
-			std::cout << times[g[i][j]] << ", ";
-		}
-		//std::cout << g[i] << std::endl;
+	for (int i = 0; i < groups.size(); ++i) {
+		for (int j = 0; j < groups[i].size(); ++j) 
+			std::cout << times[groups[i][j]] << ", ";
 		std::cout << std::endl;
 	}
 
